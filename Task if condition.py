9432207262669c72d2491b78f037a930861e659b
Maheshvadladi <132981540+Maheshvadladi@users.noreply.github.com>
2026-02-27 17:36:29 +0530

    Add files via upload

---
+++ Task if condition.py
@@ -0,0 +1,45 @@
+#Task 4
+'''name=input()
+if name=="Mahesh":
+    print("Welcome"+" "+name)
+else:
+    print("Welcome"+" "+"guest")'''
+
+'''a=["Mahesh","Manoj","Mohith","Sasidar","Ramarao"]
+b=input()
+if b in a:
+    print("Welcome",b)
+else:
+    print("Welcome guest")'''
+    
+#Task 5
+'''User_name=input("Enter username :")
+Password=int(input("Enter password :"))
+if User_name=="Mahesh":
+    if Password==1234:
+        print("Login Successful")
+    else:
+        print("Invalid Password")
+else:
+    print("Invalid credentials")'''
+
+'''User_name=input("Enter username: ")
+Password=int(input("Enter password: "))
+if (User_name=="Mahesh") and (Password==1234):
+    print("Login successful")
+else:
+    print("Invalid credentials")'''
+
+#Task 6
+'''Price=int(input("Enter the price: "))
+if Price==1200:
+    print("Redvelvet cake")
+elif Price==1000:
+    print("Choclate cake")
+elif Price==800:
+    print("Butterscotch")
+elif Price==600:
+    print("Choco Almond")
+else:
+    print("Sorry cake is not avialable")'''
+
